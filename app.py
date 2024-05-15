6da0d6e003d4a94c25e3735e419179add3c9383d
JablonskiFlp <146426227+JablonskiFlp@users.noreply.github.com>
2024-05-15 13:05:12 +0000

    Dodałem stronę zawierającą informacje dla każdego filmu z osobna, możliwość wyboru którego dnia ktoś chce obejrzeć film oraz zmieniłem system dostępu do zdjęć przez bazę danych

--- app.py
+++ app.py
@@ -1,41 +1,55 @@
-from flask import Flask, render_template
-from database import engine, load_movies_from_db, load_seances_from_db
-import pandas as pd
-import numpy as np
+from flask import Flask, render_template, request
+from database import load_movie_from_db, load_seances_for_movie_from_db, engine, load_movies_from_db, load_seances_from_db, get_unique_dates_from_db
 import os
-import datetime
 # , jsonify, request
 # from database import engine, load_jobs_from_db, add_application_to_db, load_job_from_db
 # from sqlalchemy import text
 
 app = Flask(__name__)
-seances = load_seances_from_db()
-movies = load_movies_from_db()
-
-folder_path = 'static/images'  # Ścieżka do folderu images
-image_files = os.listdir(folder_path)  # Pobieranie listy plików z folderu
-data = [os.path.join(folder_path, file)
-        for file in image_files]  # Tworzenie listy ścieżek do plików
-data.sort(key=str.lower)
-movies = movies.sort_values(
-    by="title"
-)  # dopasowanie kolumn odpowiedniego plakatu do odpowiedniego filmu
-movies['poster'] = data  # dodanie kolumny z ścieżkami do plakatów
-reference_date = np.datetime64('1970-01-01')
 
 # stworzyc slownik z kluczem jako nazwa filmu oryginalna a wartosci to lista przechowujaca zdjecia
 
-def format_time(delta):
-  seance_time = delta + reference_date
-  return f"{seance_time.hour:02d}:{seance_time.minute:02d}"
-
-
-# Zastosowanie funkcji na kolumnie 'start_time'
-seances['start_time'] = seances['start_time'].apply(format_time)
 
 @app.route("/")
 def hello_world():
-  return render_template("home.html", movies = movies, seances = seances)
+  selected_date = request.args.get('date')
+
+  movie_dates = get_unique_dates_from_db()
+  if not selected_date:
+    selected_date = "2024-06-22"
+
+  seances = load_seances_from_db(date=selected_date)
+  movies = load_movies_from_db(seances.movie_id.unique())
+  movie_genres = get_movie_genres(movies, seances)
+
+  print(selected_date)
+  return render_template("home.html",
+                         movies=movies,
+                         seances=seances,
+                         movie_dates=movie_dates,
+                         selected_date=selected_date,
+                         movie_genres=movie_genres)
+
+
+def get_movie_genres(movies, seances):
+  genre_lists = [
+      movie_genre.split(' / ') for movie_genre in movies['movie_genre'].values
+  ]
+  combined_list = []
+  for sublist in genre_lists:
+    combined_list.extend(sublist)
+  return list(set(combined_list))
+
+
+@app.route("/movie/<title>")
+def show_job(title):
+  movie = load_movie_from_db(title)
+  id = movie.movie_id.values[0]
+  seances = load_seances_for_movie_from_db(id)
+  if not title:
+    return "Not Found", 404
+  return render_template('movie_info.html', movie=movie, seances=seances)
+
 
 if __name__ == "__main__":
   app.run(host="0.0.0.0", debug=True)
--- database.py
+++ database.py
@@ -1,6 +1,8 @@
 from sqlalchemy import create_engine, text
-import os
 import pandas as pd
+import numpy as np
+import os
+import datetime
 
 db_connection_string = os.environ['DB_CONNECTION_STRING']
 
@@ -9,13 +11,63 @@ engine = create_engine(db_connection_string,
                            "ssl_ca": "/etc/ssl/cert.pem"
                        }})
 
-def load_movies_from_db():
-    with engine.connect() as conn:
-        sql_query = pd.read_sql_query("SELECT * FROM movies", conn)
-        return pd.DataFrame(sql_query)
 
-def load_seances_from_db():
+def time_formatting(data):
+    reference_date = np.datetime64('1970-01-01')
+
+    def format_time(delta):
+        seance_time = delta + reference_date
+        return f"{seance_time.hour:02d}:{seance_time.minute:02d}"
+
+    return data['start_time'].apply(format_time)
+
+
+def load_movies_from_db(movie_ids):
     with engine.connect() as conn:
-        sql_query = pd.read_sql_query("SELECT * FROM Seances", conn)
-        return pd.DataFrame(sql_query)
-load_seances_from_db()
+        movie_ids_str = ','.join(map(str, movie_ids))
+
+        sql_query = f"SELECT * FROM movies WHERE movie_id IN ({movie_ids_str})"
+        movies = pd.read_sql_query(sql_query, conn)
+
+    # Ścieżka do folderu images
+    folder_path = '../static/images/'
+
+    # Dodanie ścieżki do plakatów do danych o filmach
+    movies['poster'] = folder_path + movies['poster']
+
+    return movies
+
+
+def get_unique_dates_from_db():
+    with engine.connect() as conn:
+        # Budowanie zapytania SQL
+        sql_query = "SELECT DISTINCT movie_date FROM Seances"
+        movie_dates = pd.read_sql_query(sql_query, conn)
+        return movie_dates
+
+
+def load_seances_from_db(date=None):
+    with engine.connect() as conn:
+        # Budowanie zapytania SQL
+        sql_query = "SELECT * FROM Seances WHERE 1=1"  # Zawsze prawdziwe warunki, aby łatwo dodawać inne filtry
+        if date:
+            sql_query += f" AND movie_date = '{date}'"
+
+        # Pobieranie danych z bazy danych na podstawie zbudowanego zapytania
+        seances = pd.read_sql_query(sql_query, conn)
+        seances['start_time'] = time_formatting(seances)
+        return seances
+
+
+def load_movie_from_db(title):
+    movies = load_movies_from_db()
+    return movies[movies.title == title]
+
+
+def load_seances_for_movie_from_db(id):
+    with engine.connect() as conn:
+        sql_query = pd.read_sql_query(
+            f"SELECT * FROM Seances WHERE movie_id = '{id}'", conn)
+        seances = pd.DataFrame(sql_query)
+        seances['start_time'] = time_formatting(seances)
+        return seances
